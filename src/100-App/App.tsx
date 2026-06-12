0c4112b4ebf634b0131f1e422275a0b0fea3d464
fable54321 <timothe5@hotmail.com>
2026-06-12 13:14:50 -0400

    first commit

--- src/100-App/App.tsx
+++ src/100-App/App.tsx
@@ -3,7 +3,7 @@
 const App = () => {
   return (
     <article className="font-tertiary">
-      <h2>agenda interactif
+      <h2 className="hidden sr-only">agenda interactif
 
       </h2>
     </article>
