db2516d6e8ace8e7eb4a70e7d0d0a523ae126043
fable54321 <timothe5@hotmail.com>
2026-07-24 12:17:35 -0400

    first commit

---
+++ android/app/src/main/java/com/vegibecportail/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.vegibecportail.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vegibecportail.app',
+  appName: 'agenda',
+  webDir: 'dist'
+};
+
+export default config;
